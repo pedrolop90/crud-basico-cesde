0ded657f44ecfbd4abedce3da5417b2d0b076179
Elias Pasion <elian.pbh@gmail.com>
2023-03-12 16:05:57 -0500

    add personaje

---
+++ src/main/java/org/example/entity/PersonajeEntity.java
@@ -0,0 +1,30 @@
+package org.example.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="personaje")
+public class PersonajeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "personaje_id")
+    private Long personajeId;
+    private String nombre;
+    private Double salud;
+    private Double velocidad;
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name = "mochila_id")
+    @JsonManagedReference
+    private MochilaEntity mochila;
+
+
+}
